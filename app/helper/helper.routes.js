06434d7e429b8c70e865d4bf91980446dde9351a
unknown <sumbomatic@gmail.com>
2020-12-16 16:25:51 +0100

    update

--- app/helper/helper.routes.js
+++ app/helper/helper.routes.js
@@ -2,9 +2,7 @@ module.exports = app => {
     const helper = require("./helper.controller");
     const jwtTokenUtils = require('../helpers/jwtTokenUtils')
     const { verifyToken, isAdmin } = jwtTokenUtils;
-    // require('../Cloudinary/cloudinary.js')
-    // const upload = require('../Cloudinary/multer.js');
-
+   
         //console.log("routes")
  app.post("/branch", helper.createBranch)
  app.post("/role", helper.createRole)
