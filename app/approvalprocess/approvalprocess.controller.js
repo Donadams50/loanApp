cdf132a96a7547c26fc0cbc33f38020f58ff78d5
unknown <sumbomatic@gmail.com>
2021-01-18 10:59:13 +0100

    update

--- app/approvalprocess/approvalprocess.controller.js
+++ app/approvalprocess/approvalprocess.controller.js
@@ -196,6 +196,19 @@ exports.deleteApprovalProcess = async (req, res) => {
 }
 
 
+// delete aproval process
+exports.deleteApprovalProcess = async (req, res) => {
+    try{
+        const id = req.params.id;
+        const deletapprovalprocess= await ApprovalProcess.findByIdAndRemove(id)
+        console.log(deletapprovalprocess)
+        res.status(200).send({message:"Deleted succesfully"})
+         
+       }catch(err){
+           console.log(err)
+           res.status(500).send({message:"Error while deleting approval process "})
+       }
+}
 
 
 
