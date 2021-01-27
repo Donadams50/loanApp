1ba00d6d13dc62679dcab2fa71f497c290de1640
unknown <sumbomatic@gmail.com>
2021-01-27 12:10:36 +0100

    update

--- app/helper/helper.routes.js
+++ app/helper/helper.routes.js
@@ -5,11 +5,11 @@ module.exports = app => {
    
  app.post("/branch", verifyToken, isAdmin,  helper.createBranch)
  app.post("/role", verifyToken, isAdmin, helper.createRole)
- app.get("/branches", verifyToken, isAdmin,  helper.findAllBranches)
+ app.get("/branches", verifyToken,   helper.findAllBranches)
  app.get("/roles", verifyToken, isAdmin,   helper.findAllRoles)
  app.get("/branch/count", verifyToken, isAdmin,  helper.countBranch)
  app.delete("/branch/:id", verifyToken, isAdmin,  helper.deleteBranch)
  app.put("/branch/:id", verifyToken, isAdmin,  helper.updateBranch)
  app.post("/loantype", verifyToken, isAdmin,  helper.createLoantype)
- app.get("/loantype", verifyToken, isAdmin,  helper.findAllLoantype)
+ app.get("/loantype", verifyToken,   helper.findAllLoantype)
 }
